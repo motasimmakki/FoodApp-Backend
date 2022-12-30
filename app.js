8aeaaa82c47afe5531994d3196a30d774c98bf61
motasimmakki <makkimotasim@gmail.com>
2022-12-31 03:53:24 +0530

    :technologist:feat: Storing data from postman to mongoDB and retrieving

--- app.js
+++ app.js
@@ -2,48 +2,16 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const userRouter = express.Router();
-app.use('/auth', userRouter);
+app.use('/user', userRouter);
 const mongoose = require('mongoose');
 const { db_link } = require('./secrets');
 
 // Routing mini-app
 userRouter
-    .route("/signup")
-    .get(getSignup)
+    .route("/")
+    // .get(getSignup)
+    .get(getUsers)
     .post(postSignup);
-
-let user = [
-    {
-        id: 1,
-        name: "Apple",
-        age: 12
-    },
-    {
-        id: 2,
-        name: "Mango",
-        age: 15
-    },
-    {
-        id: 3,
-        name: "Guava",
-        age: 20
-    }
-]
-
-function getSignup(req, res) {
-    res.sendFile("public/index.html", {root: __dirname});
-}
-
-function postSignup(req, res) {
-    let {email, name, password} = req.body;
-    // This log will be console on server terminal,
-    // as it is running in backend.
-    console.log(req.body);
-    res.json({
-        msg: "User Signed Up!",
-        email, name, password
-    });
-}
 
 app.listen(5000, 'localhost', () => {
     console.log("Server is listening on port no: 5000");
@@ -83,13 +51,42 @@ const userSchema = mongoose.Schema({
 const userModel = mongoose.model("userModel", userSchema);
 
 // IIFE (Immediately Invoked Function Expressing)
-(async function createUser() {
-    let user = {
-        name: "Ahkam",
-        email: "dev.ahkam@gmail.com",
-        password: "123qwerty",
-        confirmPassword: "123qwerty"
-    }
-    let data = await userModel.create(user);
+// (async function createUser() {
+//     let user = {
+//         name: "Ahkam",
+//         email: "dev.ahkam@gmail.com",
+//         password: "123qwerty",
+//         confirmPassword: "123qwerty"
+//     }
+//     let data = await userModel.create(user);
+//     console.log(data);
+// })();
+
+// Get all users from db.
+async function getUsers(req, res) {
+    let allUsers = await userModel.find();
+    // Get specific user by Id.
+    // let allUsers = await userModel.find({name: "Ahkam"});
+    console.log(allUsers);
+    res.json({
+        msg: "Users Data is retrieved",
+        allUsers
+    });
+}
+
+function getSignup(req, res) {
+    res.sendFile("public/index.html", {root: __dirname});
+}
+
+async function postSignup(req, res) {
+    // let {email, name, password} = req.body;
+    // This log will be console on server terminal,
+    // as it is running in backend.
+    let data = req.body;
+    let user = await userModel.create(data);
     console.log(data);
-})();
+    res.json({
+        msg: "User Signed Up!",
+        user
+    });
+}
