85ad728f049fb34065e9724e3f53ea92972dfec6
motasimmakki <makkimotasim@gmail.com>
2023-01-01 15:37:51 +0530

    :technologist:feat: User login with plain password check

--- Routers/authRouter.js
+++ Routers/authRouter.js
@@ -8,6 +8,10 @@ authRouter
     .get(getSignup)    
     .post(postSignup)
 
+authRouter
+    .route("/login")
+    .post(loginUser);
+
     // Auth routes.
 function getSignup(req, res) {
     res.sendFile("public/index.html", {root: __dirname});
@@ -27,6 +31,35 @@ async function postSignup(req, res) {
         });
     } catch (err) {
         res.json({err: err.message});
+    }
+}
+
+async function loginUser(req, res) {
+    try {
+        let { email, password } = req.body;
+        let user = await userModel.findOne({
+            email: email
+        })
+        if(user) {
+            // Check if password is matching
+            if(password == user.password) {
+                res.json({
+                    msg: "User is successfully LoggedIn!"
+                });
+            } else {
+                res.json({
+                    msg: "Wrong password Entered!"
+                });
+            }
+        } else {
+            res.json({
+                msg: "User NOT found!"
+            });
+        }
+    } catch(err) {
+        res.json({
+            msg: err.message
+        });
     }
 }
 
