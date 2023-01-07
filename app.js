ff8f6a3cea05e05b5294620cacd4bf8d69f595b0
motasimmakki <makkimotasim@gmail.com>
2023-01-07 13:43:50 +0530

    :technologist:feat: Modified package.json & Added frontend build:package:

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
+const cors = require('cors');
+app.use(cors());
+app.use(express.static('public/build'));
 app.use(express.json());
 app.use(cookieParser());
+
+// app.options('*', cors()) // To allow preflight
 
 const userRouter = require('./Routers/userRouter');
 const planRouter = require('./Routers/planRouter');
@@ -29,7 +34,7 @@ app.use('/booking', bookingRouter);
 //     console.log(data);
 // })();
 
-
-app.listen(5000, 'localhost', () => {
-    console.log("Server is listening on port no: 5000");
+const port = process.env.PORT || 5000;
+app.listen(port, 'localhost', () => {
+    console.log(`Server is listening on port no: ${port}`);
 });
